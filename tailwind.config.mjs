5fa497aba41bc5729989f9fecb93249dad3c54a3
Jake B <jacobboxerman@gmail.com>
2025-01-14 22:31:12 -0600

    Got hero section looking ok for mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-gradient": "url('src/assets/Frame 1.svg')",
+      },
+      fontSize: {
+        header: "2rem",
+      },
+      fontFamily: {
+        exo: ['"Exo 2 Variable"'],
+        reddit: ['"Reddit Sans Variable"'],
+        jost: ['"Jost Variable"'],
+      },
+      gridTemplateRows: {
+        'hero': 'repeat(5, minmax(80px, auto));'
+      }
+    },
+  },
+  plugins: [],
+};
