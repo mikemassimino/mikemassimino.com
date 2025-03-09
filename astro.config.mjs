a60c1a086966adfa8daa1ec591fd91fd2195d43e
Jake B <jacobboxerman@gmail.com>
2025-03-09 15:19:43 -0400

    Set page base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jakebox.github.io/',
-  base: 'mikemassimino.com',
+  site: 'https://mikemassimino.com',
   integrations: [tailwind()]
 });
